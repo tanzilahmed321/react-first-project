3a6887acf9db908af152b4219b435f2fb89c634c
tanzil ahmed <atanzil365@gmail.com>
2024-06-29 17:24:46 +0500

    ok

---
+++ react project/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+ const App =()=>{
+
+  
+  return (
+<div>
+    <h1><i>this is my react project</i></h1>
+
+    <div>
+      <img width={200} height={200} src="https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg?size=626&ext=jpg&ga=GA1.1.1141335507.1719532800&semt=sph" alt="" />
+
+      <img width={500} height={200} src="https://img.freepik.com/free-photo/abstract-autumn-beauty-multi-colored-leaf-vein-pattern-generated-by-ai_188544-9871.jpg?size=626&ext=jpg&ga=GA1.1.2113030492.1719532800&semt=ais_user" alt="" />
+    </div>
+  </div>
+  )
+}
+
+export default App
